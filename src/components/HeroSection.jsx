55263af8c2f090c15744c70234ab8cd54f31f1b7
Adrien Da Silva Ferreira <adrien@utopios.solutions>
2024-12-16 09:28:16 +0100

    Ajout du dossier Build et Nodes Modules ?

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import "../style/HeroSection.css"
 function HeroSection () {
     return (
         <div className="HeroSection">
-            <p className="HeroGreeting">Bienvenue sur mon Portfolio</p>
+            <p className="HeroGreeting">Bienvenue sur mon magnifique Portfolio</p>
             <p className="HeroIntroduction">Je suis<br /><span>Adrien Da Silva Ferreira</span><br />Développeur Web / Low Code</p>
             <p className="HeroDescription">En alliant développement web et solutions low code, je propose des solutions numériques rapides et efficaces.</p>
         </div>
